ab3c080eaae03792d799404309caf06ac5238d7a
Muhammad-AIUB <mjubayer.aiub@gmail.com>
2025-05-02 16:07:43 +0600

    clone

--- app/interview/public-health-insights-with-sharmeen-yasmeen/page.tsx
+++ app/interview/public-health-insights-with-sharmeen-yasmeen/page.tsx
@@ -18,8 +18,8 @@ const PublicHealthInsightsWithSharmeenYasmeen = () => {
             >
               Public Health Insights with{" "}
               <Link href="">
-                <span className="bg-[#FFCE00] rounded-md font-extrabold text-[#001844] underline">
-                  N PROF. SHARMEEN YASMEEN:
+                <span className="bg-[#FFCE00] px-2 rounded-md underline">
+                  PROF. SHARMEEN YASMEEN:
                 </span>
               </Link>{" "}
               <br /> Distinguished Academic and Public Health Expert
